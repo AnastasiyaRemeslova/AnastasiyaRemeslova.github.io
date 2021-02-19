a2959dbe1245bd0da1cd6c28e557583c1de7b333
AnastasiyaRemeslova <48914914+AnastasiyaRemeslova@users.noreply.github.com>
2021-02-19 16:05:38 +0600

    Choice scene

--- js/script.js
+++ js/script.js
@@ -16,133 +16,49 @@
         document.documentElement.onclick = goFullscreen;
         document.onkeydown = goFullscreen;
   */  
-      var videoActiveJS, choice;
-      var videojsPlayers;
+var videoActiveJS, choice;
+var videojsPlayers;
+var videos;
 
-$(document).ready(function() {
-
-    choice = false;
-
-    $('.choice').hide();
-
-    videojsPlayers = [];
-
-   $.each($('video'), function(){
-        $(this).addClass('video-js vjs-default-skin');
-        videojsPlayers.push(videojsCreate($(this).get(0)));
-    })
+function videoCreate(videoJQuery) {
+    var video = {
+    id: '',
+    scene: videoJQuery.parent().attr('class').slice(0, videoJQuery.parent().attr('class').indexOf(' ')),
+    type: videoJQuery.attr('class').slice(0, videoJQuery.attr('class').indexOf(' ')),
+    vHTML: '',
+    vJQuery: '',
+    vPlayer: videojsCreate(videoJQuery.get(0)),
+};
     
-
-    $('.ch_1, .ch_2').click(function() {
-        choice = true;
-        var id = '.'+ $(this).attr('class').replace(/ch/,'v');
-        var video = $(this).parent().parent().parent().find(id);
-        playVideo(video);
-    });
-
-        var videoHTML = $('.active > .active');
-       // console.log(this.hasClass('v_main'));
-        videojsPlayers.forEach(function (player) {
-            player.on('play', function(){
-
-
-            console.log(player);
-        videoHTML = $('.active > .active');
-            
-            player.on('timeupdate', function () {
-                console.log(choice, this.currentTime(), this.duration())
-            if(!choice & this.currentTime() >= this.duration()-2) {
-                videoHTML.parent().find('.choice').fadeIn(0);
-            }
-        });
-            
-        
-        player.on('ended', function(){
-            
-            if(this.hasClass('v_1') || this.hasClass('v_2')){
-        choice = false;
-
-        var lastVideoConteiner = videoHTML.parent();
-        var lastVideo = videoHTML;
-
-        lastVideo.removeClass('active');
-        lastVideo.addClass('hide');
-
-        lastVideoConteiner.removeClass('active');
-        lastVideoConteiner.addClass('hide');
-        this.pause();
-//videoActiveJS.reset();
-        if(videoHTML.parent().next()[0]){
-        var nextVideoContainer = videoHTML.parent().next();
-        var nextVideo = nextVideoContainer.find('.v_main');
-
-        nextVideoContainer.removeClass('hide');
-        nextVideoContainer.addClass('active');
-
-        nextVideo.removeClass('hide');
-        nextVideo.addClass('active');
-
-  
-        
-        //videoActiveJS = videojsCreate();
-       // videojsFunction();
-        
-    }
+    video.id = video.vPlayer.id();
+    video.vJQuery = $('#'+video.id);
+    video.vHTML = video.vJQuery.get(0);
+    return video;
 }
 
-
-    });
-    });
-
-});
-
-    $('.v_main').on('timeupdate', function(){
-        if(!choice & this.currentTime >= this.duration-2) {
-            $(this).parent().find('.choice').fadeIn(0);
+function findVideoById(id){
+    var video;
+    $.each(videos, function(){
+        if(this.id == id){
+            video = this;
         }
     });
-/*
-    $('.v_1, .v_2').on('ended', function(){
-        choice = false;
+    return video;
+}
 
-        var lastVideoConteiner = $(this).parent();
-        var lastVideo = $(this);
-
-        lastVideo.removeClass('active');
-        lastVideo.removeClass('video-js');
-        lastVideo.addClass('hide');
-
-        lastVideoConteiner.removeClass('active');
-        lastVideoConteiner.addClass('hide');
-videoActiveJS.reset();
-console.log(videoActiveJS.parent());
-        if($(this).parent().next()[0]){
-
-        var nextVideoContainer = $(this).parent().next();
-        var nextVideo = nextVideoContainer.find('.v_main');
-
-        nextVideoContainer.removeClass('hide');
-        nextVideoContainer.addClass('active');
-
-        nextVideo.removeClass('hide');
-        nextVideo.addClass('active video-js');
-        
-        videoActiveJS = videojsCreate();
-        videojsFunction();
-        nextVideo.get(0).play();
-    }
-
+function findVideoBySceneAndType(scene, type){
+    var video;
+    $.each(videos, function(){
+        if(this.scene == scene && this.type == type){
+            video = this;
+        }
     });
-*/
-
-
-});        
-
-
-
+    return video;
+}
 
 function videojsCreate(video){
     var videoj = videojs(video, {
+      "autoplay": true,
       controlBar: {
         children: [
           "playToggle",
@@ -161,83 +77,163 @@ function videojsCreate(video){
     return videoj;
 }
 
-function videojsFunction(player){
-
-videoHTML = $('.active > .active');
-            
-            player.on('timeupdate', function () {
-
-            if(!choice & this.currentTime() >= this.duration()-2) {
-                videoHTML.parent().find('.choice').fadeIn(0);
-            }
-        });
-            
-        
-        player.on('ended', function(){
-            
-            if(this.hasClass('v_1')){
-        choice = false;
-
-        var lastVideoConteiner = videoHTML.parent();
-        var lastVideo = videoHTML;
-
-        lastVideo.removeClass('active');
-        lastVideo.addClass('hide');
-
-        lastVideoConteiner.removeClass('active');
-        lastVideoConteiner.addClass('hide');
-//videoActiveJS.reset();
-        if(videoHTML.parent().next()[0]){
-        var nextVideoContainer = videoHTML.parent().next();
-        var nextVideo = nextVideoContainer.find('.v_main');
-
-        nextVideoContainer.removeClass('hide');
-        nextVideoContainer.addClass('active');
-
-        nextVideo.removeClass('hide');
-        nextVideo.addClass('active');
-
-        this.play();
-        console.log('!!!!!!!!!!!!!!!!!!!!!!!');
-        videojsFunction(this);
-        //videoActiveJS = videojsCreate();
-       // videojsFunction();
-        
-    }
-}
-
-
-    });
- 
-}
 
 function playVideo(video) {
 
-var vClass = video.attr('class').slice(0, video.attr('class').indexOf(' '))
-var cClass = video.parent().attr('class').slice(0, video.parent().attr('class').indexOf(' '))
-var nextVideo;
-videojsPlayers.forEach(function (player) {
-    if(player.hasClass(vClass) && $('#'+player.id()).parent().hasClass(cClass)){
-        nextVideo = player;
-    }
-});
-
+    var vClass = video.attr('class').slice(0, video.attr('class').indexOf(' '))
+    var cClass = video.parent().attr('class').slice(0, video.parent().attr('class').indexOf(' '));
+    var nextVideo;
+    $.each(videos, function () {
+        var player = this.vPlayer;
+        if(player.hasClass(vClass) && $('#'+player.id()).parent().hasClass(cClass)){
+            nextVideo = player;
+        }
+    });
 
     $('.choice').fadeOut(0);
 
     var lastVideo = $('.active > .active');
     lastVideo.removeClass('active');
     lastVideo.addClass('hide');
-//videoActiveJS.reset();
-//console.log(videoActiveJS);
+
     video.removeClass('hide');
     video.addClass('active');
     nextVideo.load();
-nextVideo.play();
+    nextVideo.play();
+} 
 
-//videojsFunction(nextVideo);
-            //videoActiveJS = videojsCreate();
-}    
+$(document).ready(function() {
+
+    choice = false;
+
+    //$('.choice, .navigation').hide();
+
+    videojsPlayers = [];
+    videos = [];
+
+   $.each($('video'), function(){
+        $(this).addClass('video-js vjs-default-skin');
+
+        videos.push(videoCreate($(this)));
+        //videojsPlayers.push(videojsCreate($(this).get(0)));
+    });
+
+    $('.ch_1, .ch_2').click(function() {
+        choice = true;
+        var id = '.'+ $(this).attr('class').replace(/ch/,'v');
+        var video = $(this).parent().parent().parent().find(id);
+        playVideo(video);
+    });
+
+    var videoHTML = $('.active > .active');
+
+    $.each(videos, function () {
+        var player = this.vPlayer;
+
+        player.on('play', function(){
+
+            console.log('!!!!!!!!!!!!!', player);
+
+            videoHTML = $('.active > .active');
+                
+            player.on('timeupdate', function () {
+                if(!choice & this.currentTime() >= this.duration()-2) {
+                    videoHTML.parent().find('.choice').fadeIn(0);
+                }
+            });
+                
+            
+            player.on('ended', function(){
+                
+                if(this.hasClass('v_1') || this.hasClass('v_2')){
+                    choice = false;
+
+                    var lastVideoConteiner = videoHTML.parent();
+                    var lastVideo = videoHTML;
+
+                    lastVideo.removeClass('active');
+                    lastVideo.addClass('hide');
+
+                    lastVideoConteiner.removeClass('active');
+                    lastVideoConteiner.addClass('hide');
+                    this.pause();
+
+                    if(videoHTML.parent().next()[0]){
+                        var nextVideoContainer = videoHTML.parent().next();
+                        var nextVideo = nextVideoContainer.find('.v_main');
+
+                        nextVideoContainer.removeClass('hide');
+                        nextVideoContainer.addClass('active');
+
+                        nextVideo.removeClass('hide');
+                        nextVideo.addClass('active');
+
+                        var vClass = nextVideo.attr('class').slice(0, nextVideo.attr('class').indexOf(' '))
+                        var cClass = nextVideo.parent().attr('class').slice(0, nextVideo.parent().attr('class').indexOf(' '))
+                        var nextVideoJS;
+                        $.each(videos, function () {
+                            var player = this.vPlayer;
+                            if(player.hasClass(vClass) && $('#'+player.id()).parent().hasClass(cClass)){
+                                nextVideoJS = player;
+                            }
+
+                        });
+                    
+                        nextVideoContainer.removeClass('hide');
+                        nextVideoContainer.addClass('active');
+
+                        nextVideo.removeClass('hide');
+                        nextVideo.addClass('active');
+                        nextVideoJS.play();
+                    }
+                }
+            });
+        });
+    });
+
+    $('.v_main').on('timeupdate', function(){
+        if(!choice & this.currentTime >= this.duration-2) {
+            $(this).parent().find('.choice').fadeIn(0);
+        }
+    });
+
+    $('.button').click(function() {
+        $('.navigation').fadeIn(0);
+    });
+
+    $('.navigation > div').click(function() {
+        choice = false;
+        var scene = $('.videos').find('.'+$(this).attr('class'));
+
+        var nextVideo = findVideoBySceneAndType($(this).attr('class'), 'v_main');
+
+        var currentVideo = findVideoById($('.active > .active').attr('id'));
+        currentVideo.vPlayer.pause();
+        currentVideo.vPlayer.currentTime(0);
+
+
+        currentVideo.vJQuery.removeClass('active');
+        currentVideo.vJQuery.addClass('hide');
+
+        var currentScene = $('.videos > .active');
+
+        currentScene.removeClass('active');
+        currentScene.addClass('hide');
+
+
+
+        scene.removeClass('hide');
+        scene.addClass('active');
+
+        nextVideo.vJQuery.removeClass('hide');
+        nextVideo.vJQuery.addClass('active');
+        nextVideo.vPlayer.play();
+        $(this).parent().hide();
+    })
+});        
+
+
+
 /*
 document.documentElement.onclick = fullScreen(document.documentElement);
 document.onkeydown = goFullscreen;
